c62d0fc24c9545fe4b8b090e05a497ecda4f74b4
Georginio <132018437+Georginio-prod@users.noreply.github.com>
2025-05-28 21:32:58 +0000

    feat:create route for testing

--- myApp.js
+++ myApp.js
@@ -3,13 +3,50 @@ let app = express();
 
 // console.log("HRBP  Hello World");
 
-app.get("/json", (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-  res.send({ message: message });
+// app.get("/json", (req, res) => {
+//   let message = "Hello json";
+//   if (process.env.MESSAGE_STYLE === "uppercase") {
+//     message = message.toUpperCase();
+//   }
+//   res.send({ message: message });
+
+//   let absolutePath = __dirname + "/views/index.html";
+//   res.sendFile(absolutePath);
+// });
+
+app.get("/name", function (req, res) {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+
+  res.json({
+    name: `${firstName} ${lastName}`,
+  });
 });
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word,
+  });
+});
+
+const middleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.get("/now", middleware, (req, res) => {
+  res.send({
+    time: req.time,
+  });
+});
+
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 // app.get("/json", (req, res) => {
 //   //res.json({ message: "Hello json" });
